b0f6fb11a949dbbd5819315ec8349cb42805ffd9
Reid Shields <rcs227@lehigh.edu>
2026-04-02 01:57:08 -0400

    Dash input, beginning parry rework

--- scripts/player/states/parry_state.gd
+++ scripts/player/states/parry_state.gd
@@ -1,8 +1,9 @@
+# the state where the player successfully hits a parry
 class_name ParryState
 extends State
 
 func enter() -> void:
-	SoundManager.play_sfx("chirp")
+	player.play_sfx(player.parry_sound)
 	player.safe_play("parry")
 
 func physics_process(delta: float) -> String:
